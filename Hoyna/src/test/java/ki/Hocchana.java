4e9c8907f28a0951c60f3803da9ca6defa721631
Rahman <md.n.rahman@accenturefederal.com>
2018-05-18 15:46:07 -0400

    adding the project to the github

---
+++ Hoyna/src/test/java/ki/Hocchana.java
@@ -0,0 +1,19 @@
+package ki;
+
+import org.testng.annotations.Test;
+
+public class Hocchana {
+	
+	@Test
+	public void ttt() {
+System.out.println("dfjfkdfj");	
+		
+	}
+	
+	@Test
+	public void mamam() {
+		
+		System.out.println("Hello World");
+	}
+
+}
